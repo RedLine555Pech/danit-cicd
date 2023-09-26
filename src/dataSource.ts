afaf3130e744b96fd86db4fc657f12bc774bcc77
nykyta.yevdokymenko@chatbots.studio+1-at-261222764291 <nykyta.yevdokymenko@chatbots.studio>
2023-09-26 18:40:24 +0300

    fiex deps

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,10 +1,12 @@
+import { PostEntity } from "./dal/entity/post";
+import { UserEntity } from "./dal/entity/user";
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   name: "default",
   type: "postgres",
   url: "postgres://oxfbiixv:xYd-sxc8-IPWCLferUFiOLdCy2CQyP7M@snuffleupagus.db.elephantsql.com/oxfbiixv",
-  entities: ["src/dal/entity/*.ts"],
+  entities: [PostEntity, UserEntity],
   synchronize: false,
   migrations: ["src/dal/migrations/*.ts"],
 });
